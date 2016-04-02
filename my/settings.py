b73017034a55f923bbfa29a1b448ed064781be2c
serrrgggeee <crackkc@mail.ru>
2016-04-02 14:57:32 +0300

    options

--- my/settings.py
+++ my/settings.py
@@ -127,3 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'my/','static/'),
+)
+
